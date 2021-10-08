8acf51b6a5699d6465fb12efd3f729523356aa90
ManudeQuevedo <manudequevedo@gmail.com>
2021-10-08 15:13:01 -0500

    Project finished

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log('It works like charm');
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnOpen = document.querySelectorAll('.show-modal');
+const btnClose = document.querySelector('.close-modal');
+
+const openModal = function () {
+	modal.classList.remove('hidden');
+	overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+	modal.classList.add('hidden');
+	overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnOpen.length; i++) {
+	btnOpen[i].addEventListener('click', openModal);
+}
+
+btnClose.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
